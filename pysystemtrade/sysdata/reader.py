a948cac8c9add88b05f013d0bb3e411b5eab2517
Luis Omar <32350181+luisomar3@users.noreply.github.com>
2017-12-22 16:54:41 -0400

    Add files via upload

---
+++ pysystemtrade/sysdata/reader.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+from pandas import read_csv
+import quandl
+class Reader:
+
+    """Clase para leer de un archivo CSV"""
+
+
+    def read_csv_option(self,dirname):
+        try:
+            path = os.getcwd() + "\\" + str(dirname) + ".csv"
+            data=read_csv(path)
+
+            return data
+        except Exception as e:
+            print("Nombre incorrecto, asegurese de estar en la carpeta del proyecto")
+            print(path)
+
+    def read_quandl_option(self,activo):
+
+        try:
+            data= quandl.get(activo)
+            print(activo)
+            return data
+
+        except Exception as e:
+            print(e)
